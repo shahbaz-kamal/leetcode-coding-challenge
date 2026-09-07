f6c751dc475941b0c53cb9612ef0d7d7a1b5f78d
shahbaz-kamal <shahbazkamal384@gmail.com>
2026-09-07 15:23:46 +0600

    solved 242

--- 242. Valid Anagram/242.ts
+++ 242. Valid Anagram/242.ts
@@ -1,22 +1,22 @@
-function isAnagram(s: string, t: string): boolean {
-    if (s.length !== t.length) return false
-    const stringMap = new Map<string, number>()
+// function isAnagram(s: string, t: string): boolean {
+//     if (s.length !== t.length) return false
+//     const stringMap = new Map<string, number>()
 
-    for (let i = 0; i < s.length; i++) {
-        if (!stringMap.has(s[i])) stringMap.set(s[i], 0)
-        if (!stringMap.has(t[i])) stringMap.set(t[i], 0)
-        if (stringMap.has(s[i])) {
+//     for (let i = 0; i < s.length; i++) {
+//         if (!stringMap.has(s[i])) stringMap.set(s[i], 0)
+//         if (!stringMap.has(t[i])) stringMap.set(t[i], 0)
+//         if (stringMap.has(s[i])) {
            
-            stringMap.set(s[i], (stringMap.get(s[i]) ?? 0) + 1)
-        }
-        if (stringMap.has(t[i])) stringMap.set(t[i], (stringMap.get(t[i])??0) - 1)
-    }
-    for (const [key, value] of stringMap) {
-        if (value !== 0) return false
-    }
-    console.log(stringMap)
-    return true
-};
+//             stringMap.set(s[i], (stringMap.get(s[i]) ?? 0) + 1)
+//         }
+//         if (stringMap.has(t[i])) stringMap.set(t[i], (stringMap.get(t[i])??0) - 1)
+//     }
+//     for (const [key, value] of stringMap) {
+//         if (value !== 0) return false
+//     }
+//     console.log(stringMap)
+//     return true
+// };
 
 const finalOutput = isAnagram("rat", "car")
 
@@ -37,3 +37,20 @@ console.log(finalOutput)
 //     }
 //     return true;
 // }
+
+function isAnagram(s: string, t: string): boolean {
+    if (s.length !== t.length) return false;
+
+    const counts = new Array(26).fill(0);
+    const aCode = 'a'.charCodeAt(0);
+
+    for (let i = 0; i < s.length; i++) {
+        counts[s.charCodeAt(i) - aCode]++;
+        counts[t.charCodeAt(i) - aCode]--;
+    }
+
+    for (let i = 0; i < 26; i++) {
+        if (counts[i] !== 0) return false;
+    }
+    return true;
+}
